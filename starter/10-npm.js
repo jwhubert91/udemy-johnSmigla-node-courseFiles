2f216b662408db2454d2015668cce14b6661f97f
James Hubert <jwhubert91@gmail.com>
2022-03-14 22:35:27 -0700

    init

---
+++ starter/10-npm.js
@@ -0,0 +1,19 @@
+// npm packages
+
+// Visit npmjs.com for a registry of all npm packages
+
+// package, dependency and module are all ways of referring to an npm package
+// Weekly downloads are a good indication of whether a pkg is safe
+
+// npm - a global command that comes with node
+// npm --version ...this checks the npm version you have
+
+// npm i ...installs the package name following i to your current project
+
+// npm i -g ...installs the package name following i globally to your filesystem
+// sudo npm i -g ...on mac, this force installs (usually not a good idea)
+
+// package.json ...this is a manifest file that stores important info about your project for npm
+// Automatically generate the above file with `npm init` ...make sure you do this in the root of your project
+
+// npm packages (aka dependencies) are stored in the dependencies object in package.json

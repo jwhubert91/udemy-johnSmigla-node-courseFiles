07b23bec56a8117a5fba9153e687a19dc60683ff
James Hubert <jwhubert91@gmail.com>
2022-03-15 00:23:45 -0700

    final

--- starter/16-serverEvents.js
+++ starter/16-serverEvents.js
@@ -7,3 +7,6 @@ server.on('request', (req,res) => {
 })
 
 server.listen(6969)
+
+// http servers from the http module in node have events that extend the EventEmitter
+// You can thus use the same events methods as you would on your own custom event emitters

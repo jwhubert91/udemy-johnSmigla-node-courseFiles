f6df47984d35367de30b3b03d40feb7aa29027d0
James Hubert <jwhubert91@gmail.com>
2022-03-15 00:26:07 -0700

    init

---
+++ starter/17-streams.js
@@ -0,0 +1,10 @@
+// Streams are used to read and write data sequentially
+
+// Used often for large files
+
+// 4 types of streams in node:
+
+// writeable - write data sequentially
+// readable - read data sequentially
+// duplex - do both sequentially
+// transform - transform data while it is being read sequentially

5c89c28379f5336b1f94b745e0a34429cc19bd49
James Hubert <jwhubert91@gmail.com>
2022-03-15 20:29:20 -0700

    Init

---
+++ starter/2-express/1-httpMessages.js
@@ -0,0 +1,8 @@
+// A server is just a computer whose whole job is to serve files
+
+// HTTP headers are how you send requests to the server to interact with it
+
+// GET - gets a file from the server
+
+// STATUS CODE - these are standardized codes that tell you the status of your http request
+

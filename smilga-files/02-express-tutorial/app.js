03e3c6d2d24b4c637efb779a2ad86ce0c6873548
James Hubert <jwhubert91@gmail.com>
2022-03-17 10:50:47 -0700

    init

--- smilga-files/02-express-tutorial/app.js
+++ smilga-files/02-express-tutorial/app.js
@@ -1,19 +1,9 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const { products } = require('./data')
 
-// A static asset is a file that the server never has to change.
-app.use(express.static('./public'))
-
-// app.get('/',(req,res)=> {
-//   res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-//   adding to static assets
-//   server side rendering
-// })
-
-app.all('*',(req,res)=> {
-  res.status(404).send('Resource not found.')
+app.get('/',(req,res)=> {
+  res.json(products)
 })
 
 app.listen(6969, ()=> {
---
+++ smilga-files/02-express-tutorial/express-staticFiles.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// A static asset is a file that the server never has to change.
+app.use(express.static('./public'))
+
+// app.get('/',(req,res)=> {
+//   res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+//   adding to static assets
+//   server side rendering
+// })
+
+app.all('*',(req,res)=> {
+  res.status(404).send('Resource not found.')
+})
+
+app.listen(6969, ()=> {
+  console.log('Server is listening on port 6969')
+})
+
+// NOTES - API vs SSR
+// API - JSON
+// SSR - Template
+// Send data
+// Send template
+// res.json() - to serve json over api
+// res.render() - to serve template with server side rendering
